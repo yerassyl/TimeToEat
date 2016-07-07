fdc74cdf35d4f2caa8e3bda2e124aaf5562c42bb
yerassyl <erasildias@gmail.com>
2016-07-07 15:39:30 +0600

    Initial Commit

---
+++ TimeToEat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeToEat
+//
+//  Created by User on 07.07.16.
+//  Copyright © 2016 yerassyl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
